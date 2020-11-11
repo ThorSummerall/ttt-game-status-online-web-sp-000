a4ab530afd0f66fe7bafb5d939b1b6c1cae68901
Thor Summerall <thor.summerall@gmail.com>
2020-11-11 15:16:28 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,8 +3,12 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+<<<<<<< HEAD
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
+=======
+WIN_COMBINATIONS=[
+>>>>>>> 48ee0f5345f080c389f25c1f0cb07736a1f88ead
   [0,1,2],
   [3,4,5],
   [6,7,8],
@@ -14,6 +18,7 @@ WIN_COMBINATIONS = [
   [0,4,8],
   [6,4,2]
 ]
+<<<<<<< HEAD
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
     
@@ -37,10 +42,32 @@ end
 def full?(board)
   board.all? do |index|
     index == "X" || index == "O"
+=======
+
+def won?(board)
+  WIN_COMBINATIONS.each do |combo|
+    index_1 = combo[0]
+    index_2 = combo[1]
+    index_3 = combo[2]
+    space_1 = board[index_1]
+    space_2 = board[index_2]
+    space_3 = board[index_3]
+    if space_1 == space_2 && space_2 == space_3 && position_taken?(board, index_1)
+return combo
+    end
+  end
+  false
+end
+
+def full?(board)
+  board.all? do |element|
+    element == "X" || element == "O"
+>>>>>>> 48ee0f5345f080c389f25c1f0cb07736a1f88ead
   end
 end
 
 def draw?(board)
+<<<<<<< HEAD
   if !won?(board) && full?(board)
     return true
   else
@@ -64,4 +91,19 @@ def winner(board)
   else
     return "O"
   end
+=======
+  !(won?(board)) && full?(board)
+end
+
+def over?(board)
+  won?(board) || full?(board) || draw?(board)
+end
+
+def winner(board)
+  if combo = won?(board)
+  position_1 = combo[0]
+  element_1 = board[position_1]
+else nil
+end
+>>>>>>> 48ee0f5345f080c389f25c1f0cb07736a1f88ead
 end
